3e586c5064cc3fe9c10aad47804574b40f081427
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-16 16:10:39 +0530

    returning array index

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const b = {
-    name:"Vivek",
-    f: function(){
-      var self = this;
-      console.log(this.name);
-      (()=>{
-        console.log(this.name);
-        console.log(self.name);
-      })();
-    }
+function bigFunc(element){
+    let newArray = new Array(700).fill('♥');
+    return newArray[element]
   }
-  b.f();
+  
+  console.log(bigFunc(599)); // Array is created
+  console.log(bigFunc(670)); // Array is created again
 
-  //result
-//"Vivek"
-// undefined
-// "Vivek" 
-  //Only in the IIFE inside the function f, this keyword refers to the global/window object
+//   result
+//   ♥ ////it is from 599 index
+//   ♥ //it is from 670 index
+
+
+// This code can be modified by using closures,
+function biggFunc(){
+    let newArray = new Array(700).fill('♥');
+    return (element) => newArray[element];
+  }
+  
+  let getElement = biggFunc(); // Array is created only once
+  console.log(getElement(599));
+  console.log(getElement(670)); 
