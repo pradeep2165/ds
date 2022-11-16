d6be8228f26891b0505b91138a7b40b0bff08e4c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-16 17:41:11 +0530

    anagram program

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function rotateRight(arr,rotations){
-    if(rotations == 0) return arr;
-    for(let i = 0; i < rotations;i++){
-      let element = arr.pop();
-      arr.unshift(element);
-    }
-    return arr;
-  }
-  console.log(rotateRight([2, 3, 4, 5, 7], 3)); // Return [4,5,7,2,3]
-  console.log(rotateRight([44, 1, 22, 111], 5)); // Returns [111,44,1,22] 
+function isAnagram(one, two){
+    let a = one.toLowerCase().split("").sort().join("");
+    let b = two.toLowerCase().split("").sort().join("");
+    return a===b;
+}
+console.log(isAnagram('pradeep', 'eepdarp'));
+
+//anagram program
