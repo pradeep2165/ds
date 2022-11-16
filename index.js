7739edf168cf90e05483e579342d3d1e2f8289d3
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-16 17:07:24 +0530

    git settimeout in clousers

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-function bigFunc(element){
-    let newArray = new Array(700).fill('♥');
-    return newArray[element]
-  }
-  
-  console.log(bigFunc(599)); // Array is created
-  console.log(bigFunc(670)); // Array is created again
+function randfunc(){
+    for(var i = 0; i< 2; i++){
+        (function(i){
+            setTimeout(() => {
+                console.log(i)
+            }, 1000);
+        })(i)
+    }
+}
 
-//   result
-//   ♥ ////it is from 599 index
-//   ♥ //it is from 670 index
-
-
-// This code can be modified by using closures,
-function biggFunc(){
-    let newArray = new Array(700).fill('♥');
-    return (element) => newArray[element];
-  }
-  
-  let getElement = biggFunc(); // Array is created only once
-  console.log(getElement(599));
-  console.log(getElement(670)); 
+randfunc();
