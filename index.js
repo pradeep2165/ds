1a2f1652ca8f552b160e747ce90c9a5614a123ea
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-17 00:30:55 +0530

    temp calculator

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-const b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
 
-for (let i = 0; i < 10; i++) {
-  setTimeout(() => console.log(b[i]), 1000);
-}
-
-for (var i = 0; i < 10; i++) {
-  setTimeout(() => console.log(b[i]), 1000);
-}
-
-//since let has block scope, so it is fetch one by one
-// result 
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
-// 10
-
-//since var has global/local scope, so it is increment first and then check. that is why i = 10 and b's 10 th index is undefined
-//result
-// undefined
-// undefined
-// undefined
-// undefined
-// undefined
-// undefined
-// undefined
-// undefined
-// undefined
-// undefined
+  console.log(toCelsius(-40))
