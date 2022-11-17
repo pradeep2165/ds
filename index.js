445c4f569e8b784e3cc16afc990cd060ee723ad2
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-17 19:43:43 +0530

    cartisan product algo

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-function mergeSort(arr){
-    if(arr.length < 2){
-        return arr
-    }
-    const mid = Math.floor(arr.length/2);
-    const leftArr = arr.slice(0, mid);
-    const rightArr = arr.slice(mid);
-    return merge(mergeSort(leftArr), mergeSort(rightArr));
-}
-function merge(leftArr, rightArr){
-    const sortArr = [];
-    while(leftArr.length && rightArr.length){
-        if(leftArr[0] <= rightArr[0]){
-            sortArr.push(leftArr.shift());
-        }else{
-            sortArr.push(rightArr.shift()) 
+function cartisanProduct(arr1, arr2){
+    const result =[];
+    for(let i = 0; i< arr1.length; i++){
+        for(let j = 0; j < arr2.length; j++){
+            result.push([arr1[i], arr2[j]])
         }
     }
-    return [...sortArr, ...leftArr, ...rightArr]
+    return result;
 }
 
-const arr = [8,20,-2,4,-6]
-console.log(mergeSort(arr));
+const arr1 = [1,2];
+const arr2 = [3,4,5];
 
-//big-0 o(nlogn)
+console.log(cartisanProduct(arr1, arr2));
 
-//mergesort algo
+//cartisan product
