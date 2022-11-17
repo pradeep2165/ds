1faeb5f45db41e5a04b4111eefd9268308ed1917
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-17 16:37:37 +0530

    binary search

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-function linearSearch(arr, target){
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]==target)return i
+function binarySearch(arr, target){
+    let leftIndex = 0;
+    let rightIndex = arr.length-1;
+
+    while(leftIndex <= rightIndex){
+        let middleIndex = Math.floor((leftIndex + rightIndex)/2)
+        if(target === arr[middleIndex]){
+            return middleIndex;
+        }
+        if(target < arr[middleIndex]){
+            rightIndex = middleIndex -1
+        }else{
+            leftIndex = middleIndex + 1
+        }
     }
-    return -1;
+    return -1
+
 }
 
-console.log(linearSearch([8,3,2,4,5,6], 6));
+console.log(binarySearch([-5,2,4,6,10], 6))
 
-//linear search algo
+//big-o o(log(n))
+//binary search algo
