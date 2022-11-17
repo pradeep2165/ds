4b2fce82f8b849d196ca5eeb6a82d40e50b8d275
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-17 19:04:38 +0530

    Quick sort algo

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
- function insertionSort(arr){
-    for(let i = 1; i < arr.length; i++){
-        let numberToIndex = arr[i];
-        let j = i-1;
-        while(j >= 0 && arr[j] > numberToIndex){
-            arr[j+1] = arr[j];
-            j = j-1;
-            
-        }
-        arr[j+1] = numberToIndex
-        
+function quickSort(arr){
+    if(arr.length <2){
+        return arr;
     }
+    let pivot  = arr[arr.length -1];
+    let left =[];
+    let right =[];
+    for(let i = 0; i < arr.length - 1; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i]);
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
 }
 
-const arr = [8, 20, -2, 4, -6]
-insertionSort(arr);
-console.log(arr);
+const arr = [8,20,-2,4,-6]
+console.log(quickSort(arr));
 
-// [8, 20, -2,4, -6]
-// [8, 20, -2, 4, -6]
-// [8, 20, 20, 4,  -6]
-// [8, 8, 20, 4, -6]
-// [-2, 8, 20, 4, -6]
-// [-2, 8, 20, 20, -6]
-// [-2, 8, 8, 20, -6]
-// [-2, 4, 8, 20, -6]
-// [-2, 4, 8, 20, 20]
-// [-2, 4, 8, 8, 20]
-// [-2, 4, 4, 8, 20]
-// [-2, -2, 4, 8, 20]
-// [-6, -2, 4, 8, 20]
-
-//insertionSort algo
+//worst case o(n^2);
+//avg case o(nlogn);
