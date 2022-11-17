5d503dbe4985c5f0d66f94400d10c5468e89f001
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-17 17:03:41 +0530

    recoursive binary search

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-function binarySearch(arr, target){
-    let leftIndex = 0;
-    let rightIndex = arr.length-1;
-
-    while(leftIndex <= rightIndex){
-        let middleIndex = Math.floor((leftIndex + rightIndex)/2)
-        if(target === arr[middleIndex]){
-            return middleIndex;
-        }
-        if(target < arr[middleIndex]){
-            rightIndex = middleIndex -1
-        }else{
-            leftIndex = middleIndex + 1
-        }
+function recursiveBinarySearch(arr, target){
+    return search(arr, target, 0, arr.length-1);
+}
+function search(arr, target, leftIndex, rightIndex){
+    if(leftIndex > rightIndex){
+        return -1
     }
-    return -1
-
+    let middleIndex = Math.floor((leftIndex + rightIndex)/2);
+    if(target === arr[middleIndex]){
+        return middleIndex;
+    }
+    if(target < arr[middleIndex]){
+        return search(arr, target, leftIndex, middleIndex - 1)
+    }else{
+        return search(arr, target, middleIndex + 1, rightIndex)
+        
+    }
 }
 
-console.log(binarySearch([-5,2,4,6,10], 6))
+console.log(recursiveBinarySearch([-5,2,4,6,10], 6))
 
 //big-o o(log(n))
-//binary search algo
+//recoursive binary search algo
