ec2ef8cae55986b967b6ca28940e568ad43db0b1
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-17 18:31:01 +0530

    insertionSort algo

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-function bubbleSort(arr){
-    let swapped;
-do{
-    swapped =false
-    for(let i=0; i < arr.length-1; i++){
-        if(arr[i] > arr[i+1]){
-            let temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp
-            swapped = true
+ function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        let numberToIndex = arr[i];
+        let j = i-1;
+        while(j >= 0 && arr[j] > numberToIndex){
+            arr[j+1] = arr[j];
+            j = j-1;
+            
         }
+        arr[j+1] = numberToIndex
+        
     }
-
-}while(swapped)
-return arr;
 }
 
-const arr  = [8,-1, 20,-2,4,-6]
+const arr = [8, 20, -2, 4, -6]
+insertionSort(arr);
+console.log(arr);
 
-console.log(bubbleSort(arr))
+// [8, 20, -2,4, -6]
+// [8, 20, -2, 4, -6]
+// [8, 20, 20, 4,  -6]
+// [8, 8, 20, 4, -6]
+// [-2, 8, 20, 4, -6]
+// [-2, 8, 20, 20, -6]
+// [-2, 8, 8, 20, -6]
+// [-2, 4, 8, 20, -6]
+// [-2, 4, 8, 20, 20]
+// [-2, 4, 8, 8, 20]
+// [-2, 4, 4, 8, 20]
+// [-2, -2, 4, 8, 20]
+// [-6, -2, 4, 8, 20]
 
-//big-0 = o(n^2);
+//insertionSort algo
